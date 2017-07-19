aa95e4203d93e6b760b2b609c36c885b41586834
Luís Geraldo de Ramos <gegeramos72@gmail.com>
2017-07-19 00:56:44 -0300

    user

--- src/main/java/com/luisgr/telegrambot/data/User.java
+++ src/main/java/com/luisgr/telegrambot/data/User.java
@@ -1,15 +1,23 @@
 package com.luisgr.telegrambot.data;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-public class User {
+@Table(name = "user")
+public class User implements Serializable{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
 
